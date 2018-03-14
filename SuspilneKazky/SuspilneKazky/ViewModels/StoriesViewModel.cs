51fc5023a0b72c91935c7befda029763ce9e2745
Andrii Krupka <andrii.krupka@outlook.com>
2018-03-14 14:17:39 +0200

    add stories collection view controller with simple view cell

--- SuspilneKazky/SuspilneKazky/ViewModels/StoriesViewModel.cs
+++ SuspilneKazky/SuspilneKazky/ViewModels/StoriesViewModel.cs
@@ -1,12 +1,50 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Threading.Tasks;
 using MvvmCross.Core.ViewModels;
+using SuspilneKazky.DataAccess;
+using SuspilneKazky.Models;
 
 namespace SuspilneKazky.ViewModels
 {
+    [PropertyChanged.AddINotifyPropertyChangedInterface]
     public class StoriesViewModel : MvxViewModel
     {
-        public StoriesViewModel()
+        private readonly IMediaProvider _mediaProvider;
+
+        public StoriesViewModel(IMediaProvider mediaProvider)
         {
+            _mediaProvider = mediaProvider;
+            PlaySongCommand = new MvxCommand<StorySong>(PlaySoundExecute);
         }
-    }
+
+		private void PlaySoundExecute(StorySong storySong)
+        {
+            if (storySong != null)
+            {
+                Debug.WriteLine(storySong.Name);
+            }
+        }
+
+        public override Task Initialize()
+		{
+            this.LoadData();
+            return base.Initialize();
+		}
+
+        public List<StorySong> Items { get; set; }
+        public MvxCommand<StorySong> PlaySongCommand { get; set; }
+        private async void LoadData()
+        {
+            try
+            {
+                Items = await _mediaProvider.LoadStoriesAsync();
+            }
+            catch
+            {
+                
+            }
+        }
+	}
 }
