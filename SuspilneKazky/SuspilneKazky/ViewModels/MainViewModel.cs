d3a1f98a2e85bb398d973d39e722b526d38759b0
Andrii Krupka <andrii.krupka@outlook.com>
2018-03-14 12:20:32 +0200

    add mvvmcross startup to ios project

---
+++ SuspilneKazky/SuspilneKazky/ViewModels/MainViewModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using MvvmCross.Core.ViewModels;
+
+namespace SuspilneKazky.ViewModels
+{
+    public class MainViewModel : MvxViewModel
+    {
+        public MainViewModel()
+        {
+            ShowInitialViewModelsCommand = new MvxCommand(ShowInitialViewModelsExecute);
+        }
+
+        public MvxCommand ShowInitialViewModelsCommand { get; private set; }
+
+        private void ShowInitialViewModelsExecute()
+        {
+            ShowViewModel<StoriesViewModel>();
+            ShowViewModel<RadioViewModel>();
+        }
+    }
+}
---
+++ SuspilneKazky/iOS/Setup.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using MvvmCross.Core.ViewModels;
+using MvvmCross.iOS.Platform;
+using MvvmCross.iOS.Views.Presenters;
+using UIKit;
+
+namespace SuspilneKazky.iOS
+{
+    public class Setup : MvxIosSetup
+    {
+        public Setup(MvxApplicationDelegate applicationDelegate, UIWindow window)
+            : base(applicationDelegate, window)
+        {
+        }
+
+        public Setup(MvxApplicationDelegate applicationDelegate, IMvxIosViewPresenter presenter)
+            : base(applicationDelegate, presenter)
+        {
+        }
+
+        protected override IMvxApplication CreateApp()
+        {
+            return new App();
+        }
+
+        protected override void FillValueConverters(MvvmCross.Platform.Converters.IMvxValueConverterRegistry registry)
+        {
+            
+        }
+
+
+        protected override IMvxIosViewPresenter CreatePresenter()
+        {
+            var presenter = new MvxIosViewPresenter(ApplicationDelegate, Window);
+
+            return presenter;
+        }
+    }
+
+    public class App :  BaseApp
+    {
+        protected override void RegisterSpecificServices()
+        {
+            base.RegisterSpecificServices();
+
+        }
+
+        protected override void OnInitialazing()
+        {
+         
+        }
+
+    }
+}
