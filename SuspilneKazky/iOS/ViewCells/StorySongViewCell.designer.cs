af493e69900b3e4115587627b246a39263ca0f4f
Andrii Krupka <andrii.krupka@outlook.com>
2018-04-04 10:17:48 +0300

    update nuget packages

--- SuspilneKazky/iOS/ViewCells/StorySongViewCell.designer.cs
+++ SuspilneKazky/iOS/ViewCells/StorySongViewCell.designer.cs
@@ -1,10 +1,11 @@
-﻿// WARNING
+// WARNING
 //
-// This file has been generated automatically by Visual Studio to store outlets and
-// actions made in the UI designer. If it is removed, they will be lost.
-// Manual changes to this file may not be handled correctly.
+// This file has been generated automatically by Visual Studio from the outlets and
+// actions declared in your storyboard file.
+// Manual changes to this file will not be maintained.
 //
 using Foundation;
+using System;
 using System.CodeDom.Compiler;
 
 namespace SuspilneKazky.iOS.ViewCells
@@ -14,7 +15,7 @@ namespace SuspilneKazky.iOS.ViewCells
     {
         [Outlet]
         UIKit.UIImageView PreviewImage { get; set; }
-        
+
         void ReleaseDesignerOutlets ()
         {
             if (PreviewImage != null) {
